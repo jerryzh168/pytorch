4148d9a06925e730e35d125e0d4ac81ea5304713
Jerry Zhang <jerryzh@fb.com>
2019-04-10 16:16:49 -0700

    Add Quantized Backend (#18546)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/18546
    
    We'll expose all combinations of various ways of quantization in the top level dispatch key, that is we have AffineCPUTensor, PerChannelAffineCUDATensor, etc.
    
    QTensor method added:
    - is_quantized()
    - item()
    
    Differential Revision: D14637671
    
    fbshipit-source-id: dcf9c2e21ef2a721e74c1b963405d7b482e26084

--- aten/src/ATen/quantized/Quantizer.cpp
+++ aten/src/ATen/quantized/Quantizer.cpp
@@ -43,8 +43,9 @@ inline QTensor new_qtensor(
       allocator->allocate(nelements * dtype.itemsize()),
       allocator,
       /*resizable=*/true);
+  // TODO: get TensorTypeId from quantizer
   auto tensor = detail::make_tensor<QTensorImpl>(
-      storage_impl, at::CPUTensorId(), is_variable, quantizer);
+      storage_impl, at::QuantizedCPUTensorId(), is_variable, quantizer);
   get_qtensorimpl(tensor)->set_sizes_contiguous(sizes);
   return tensor;
 }
